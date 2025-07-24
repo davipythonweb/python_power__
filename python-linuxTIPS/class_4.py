54536d1d2ef20d68d84e96ea49f677f187cdf8c4
davipythonweb <galaxys3tab32@gmail.com>
2025-07-24 04:26:51 -0300

    comandos in terminal

--- python-linuxTIPS/class_4.py
+++ python-linuxTIPS/class_4.py
@@ -1,1 +1,8 @@
 # aprendendo python pelo terminal!
+
+# para executar um comando
+#  no terminal => python -c <comando>
+
+# para executar um modulo => python -m site
+# OBS: o modulo (site) mostra
+# onde o python e os pacotes dele estao instalados.
