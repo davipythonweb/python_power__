5a49c02891b4f99efe44dc2a0b50031c312679d8
davipythonweb <galaxys3tab32@gmail.com>
2025-07-24 05:00:08 -0300

    terminal com python

--- python-linuxTIPS/class_4.py
+++ python-linuxTIPS/class_4.py
@@ -1,3 +1,5 @@
+#!usr/bin/venv python3
+
 # aprendendo python pelo terminal!
 
 # para executar um comando
@@ -6,3 +8,10 @@
 # para executar um modulo => python -m site
 # OBS: o modulo (site) mostra
 # onde o python e os pacotes dele estao instalados.
+
+# python -V  => versao do python
+# python -VV => versao e data de compilacao
+# python --help
+# python -m turtledemo => interface grafica com tk
+# python => ferramenta do interpretador python
+#   (REPL) R=ler,E=interpretar,P=imprimir,L=loop
