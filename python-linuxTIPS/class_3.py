d7dae246e1fb2c359b8b51df84b537bdcbfa594f
davipythonweb <galaxys3tab32@gmail.com>
2025-07-24 04:04:40 -0300

    ecosistema python

--- python-linuxTIPS/class_3.py
+++ python-linuxTIPS/class_3.py
@@ -4,4 +4,8 @@
 # criador do python => Guido Vam rossum
 
 # CPython eh a implementação oficial do python!
+# por baixo dos panos ele usa a linguagem C.
 
+# ECOSSISTEMA PYTHON=>
+# -Comunidades, PSF(python software fondation),
+# , pacotes e ferramentas, PyPI(Py.Org), VOcê!
