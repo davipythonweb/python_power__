2adced621072c04e3b5570807fe1525f3edeb1ae
davipythonweb <galaxys3tab32@gmail.com>
2025-07-27 13:46:09 -0300

    update script

--- python-linuxTIPS/python-base/class_5.py
+++ python-linuxTIPS/python-base/class_5.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
+# primeira linha diz: rode o env antes de rodar o programa!
+
 # aula 5 Linux Tips python BASE
 print('print: davi'.upper())
 print(f'print:' , 56*7)
+
+# saber onde o python esta instalado no linux => which python
+# saber onde o python esta instalado no windows => where python
