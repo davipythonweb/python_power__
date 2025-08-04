895e6e212ef8276caeb10cc9a775c82998290dbe
davipythonweb <galaxys3tab32@gmail.com>
2025-08-04 18:27:00 -0300

    class 7 update

--- python-linuxTIPS/python-base/class_6.py
+++ python-linuxTIPS/python-base/class_6.py
@@ -5,6 +5,11 @@
 Ola mundo multi linhas
 
 """
+__version__ = "0.0.1"
+__author__ = "Davi"
+__licence__ = "Unlicense"
+
+# Dunder = OS ANDERLINES
 
 # Este programa imprime o Ola Mundo
 print("Ola Mundo")
---
+++ python-linuxTIPS/python-base/class_7.py
@@ -0,0 +1,4 @@
+# Aula 7
+
+# Expression = Instruçao que espera 
+#              um valor de retorno
