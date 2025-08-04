737324af76f4ab4bb3c972957d31648bc80947c1
davipythonweb <galaxys3tab32@gmail.com>
2025-08-04 18:45:58 -0300

    create more exemple

--- python-linuxTIPS/python-base/class_7.py
+++ python-linuxTIPS/python-base/class_7.py
@@ -2,3 +2,17 @@
 
 # Expression = Instruçao que espera 
 #              um valor de retorno
+# exemplo = 1 + 1 =2
+#           8 > 10 = Falso
+#           8 < 10 = verdadeiro
+
+
+# Statement = Instrução que prepara
+#  o interpretador para uma determinada
+# tarefa, mas nao retorna valor.
+# exemplo = if, else, def, for, while, pass
+
+
+# Assignment = instruçao que pega o retorno
+#  de uma expressao e processa o seu valor
+# com o intuito de armazenar.
