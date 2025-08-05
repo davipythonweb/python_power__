5d0a6050e9cf80226f4ed83ad5dabefa4cfdd225
davipythonweb <galaxys3tab32@gmail.com>
2025-08-05 20:00:29 -0300

    create class13

---
+++ python-linuxTIPS/python-base/medio/class_13.py
@@ -0,0 +1,9 @@
+# Aula 13
+
+# Formatação de textos
+
+# concatenando str com float
+# nome = "Davi"
+# sando = 30.0
+# "O saldo do " + nome + " eh de " + str(saldo)
+# resposta: 'O saldo do Davi eh de 30.0'
