456959be91e20c17109f24e737f08f2b3edcb862
davipythonweb <galaxys3tab32@gmail.com>
2025-08-05 00:42:21 -0300

    update class10

--- python-linuxTIPS/python-base/medio/class_10.py
+++ python-linuxTIPS/python-base/medio/class_10.py
@@ -19,7 +19,11 @@
 
 
 
-
+# Tipos de dados primarios = Scalar types(representam um unico valor)
+# exemplo: int
+# exemplo para saber os metodos do objeto: dir(int)
+# exemplo para saber quantos metodos há: len(dir(int)) = 74
+# OBS: os metodos que nao começam com: _  sao metodos PUBLICOS. como:'real'
 
 
 
