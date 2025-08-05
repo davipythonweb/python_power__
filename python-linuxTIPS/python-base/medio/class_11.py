79be6bb51337420cf03f5e251cf57e61b49d08cd
davipythonweb <galaxys3tab32@gmail.com>
2025-08-05 01:03:57 -0300

    create class11

---
+++ python-linuxTIPS/python-base/medio/class_11.py
@@ -0,0 +1,17 @@
+# aula 11
+
+# tipos de dados primarios = Scalar types
+
+# float
+# exemplo: 2.5
+# exemplo para saber quantos metodos há: len(dir(float)) = 59
+
+
+# bool
+# exemplo: True ou False
+# exemplo para saber quantos metodos há: len(dir(bool)) = 74
+
+
+# NoneType
+# exemplo: None
+# exemplo para saber quantos metodos há: len(dir(NoneType)) = 25
