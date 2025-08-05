0c3739e11cd746f65852a0066e73848a12e8bb34
davipythonweb <galaxys3tab32@gmail.com>
2025-08-04 22:27:03 -0300

    create class10

---
+++ python-linuxTIPS/python-base/medio/class_10.py
@@ -0,0 +1,8 @@
+# Aula 10 - tipos de dados
+
+# bin(65) = 0b1000001
+# OBS: todo numero é salvo como binário
+
+# chr(65) = 'A'
+# OBS:toda letra é convertida para numero(pela tabela ASCII) e
+#  depois salvo em binário
