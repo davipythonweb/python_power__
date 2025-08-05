4a47bffbc193b4f48d1deed8fad4156580a92c07
davipythonweb <galaxys3tab32@gmail.com>
2025-08-05 03:22:07 -0300

    update class 12

--- python-linuxTIPS/python-base/medio/class_12.py
+++ python-linuxTIPS/python-base/medio/class_12.py
@@ -7,3 +7,12 @@
 # É utilizado a TABELA ASCII para converter os dados str
 # existe outra tabela chamada : unicode , para simbulos e pictograph
 # atualmente exsite a tabela : utf8 que tem 8bits
+
+# unicode = 🐍 = U+1F40D
+
+# serializaçao
+# converte o emotion para hex
+# ENCODE:  emotion = snake.encode("utf-8") = b'\xf0\x9f\x90\x8d'
+
+# decodifica o emotion que estava em hexadecimal para utf-8
+# DECODE:  emotion.decode() = '🐍'
