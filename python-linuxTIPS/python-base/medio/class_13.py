9a5e155d336dc45f4ac7a5ddb2dcf74f317bc2c2
davipythonweb <galaxys3tab32@gmail.com>
2025-08-05 20:07:36 -0300

    update class13

--- python-linuxTIPS/python-base/medio/class_13.py
+++ python-linuxTIPS/python-base/medio/class_13.py
@@ -4,6 +4,12 @@
 
 # concatenando str com float
 # nome = "Davi"
-# sando = 30.0
+# saldo = 30.0
 # "O saldo do " + nome + " eh de " + str(saldo)
 # resposta: 'O saldo do Davi eh de 30.0'
+
+# Interpolaçao
+# nome = "Davi"
+# saldo = 30.0
+# template = "O saldo do %s eh o total de %f"
+# resposta: 'O saldo do Davi eh o total de 30.000000'
