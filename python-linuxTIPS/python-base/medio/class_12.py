05ce1421ee58e5db0db9e5b7b5925794c4d1932a
davipythonweb <galaxys3tab32@gmail.com>
2025-08-05 01:50:50 -0300

    update class 12

---
+++ python-linuxTIPS/python-base/medio/class_12.py
@@ -0,0 +1,9 @@
+# aula 12
+
+# tipos de dados = textos
+
+# str , plural = strings -> varios caracteres
+# exemplo para saber quantos metodos há: len(dir(str)) = 81
+# É utilizado a TABELA ASCII para converter os dados str
+# existe outra tabela chamada : unicode , para simbulos e pictograph
+# atualmente exsite a tabela : utf8 que tem 8bits
