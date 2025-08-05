67d23ee5dd72380e4d76857e7beaa4e1eac05d21
davipythonweb <galaxys3tab32@gmail.com>
2025-08-04 22:45:05 -0300

    update class10

--- python-linuxTIPS/python-base/medio/class_10.py
+++ python-linuxTIPS/python-base/medio/class_10.py
@@ -1,8 +1,33 @@
 # Aula 10 - tipos de dados
+# UTILIZANDO O ipython
 
-# bin(65) = 0b1000001
-# OBS: todo numero é salvo como binário
+# bin(65) resposta= 0b1000001
+# OBS: todo numero é salvo como binário, atravez da inferência de tipos
 
-# chr(65) = 'A'
+# chr(65) resposta= 'A'
 # OBS:toda letra é convertida para numero(pela tabela ASCII) e
 #  depois salvo em binário
+
+#  id(65) resposta= 140703442358712
+# retornar o numero identificador do dado
+
+# type(65) resposta= int
+
+
+# UM OBJETO É:
+# um endereço de memoria, uma posiçao, um tipo, um valor
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
