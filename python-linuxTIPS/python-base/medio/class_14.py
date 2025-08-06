14f87d1e6e0bfee56cc4d620ddd8f27dd7f2cbda
davipythonweb <galaxys3tab32@gmail.com>
2025-08-06 00:44:10 -0300

    finalizada class14 tuples

---
+++ python-linuxTIPS/python-base/medio/class_14.py
@@ -0,0 +1,17 @@
+# aula 14
+
+# TIPOS DE DADOS COMPOSTOS===>
+
+# Tuple  (IMUTAVEL,nao pode ser auterada)
+# exemplo:dados = ("Davi",30, True, None, 18.12)
+# type(dados)
+# resposta: tuple
+# saber o numero de metodos: len(dir(tuple)) 
+# resposta: A TUPLA TEM 35 metodos
+
+# unpacking de tupla (desempacotamento)
+# exemplo: pontos = 2, 1, 99
+# x, y, z = pontos
+
+# desenpacotando(o restante foi atribuido no _)
+# x, *_ = pontos
