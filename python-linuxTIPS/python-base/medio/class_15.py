2dd66a1b938cb4e0b7a5c2f0c7eec0db54d53ccf
davipythonweb <galaxys3tab32@gmail.com>
2025-08-18 18:41:03 -0300

    update class 15

--- python-linuxTIPS/python-base/medio/class_15.py
+++ python-linuxTIPS/python-base/medio/class_15.py
@@ -1,2 +1,22 @@
-# aula15 - Listas
+# aula15 - Linux-tips - Listas
 
+# OBS:AS LISTAS SAO MUTAVEIS
+# OBS:As listas podem ser somadas, simplesmente com +
+
+# exemplo para saber
+#  quantos metodos há nas listas: len(dir(list)) = 48 metodos
+
+
+# criando um objeto do tipo lista:
+# users = []
+
+
+
+# EXEMPLO DE ITERAÇAO COM LISTAS:
+
+# resultado = []
+
+# for x in range(9):
+#             resultado.append(x * 4)
+
+# resultado = [0, 4, 8, 12, 16, 20, 24, 28, 32]
