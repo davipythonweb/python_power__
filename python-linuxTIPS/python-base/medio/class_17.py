631efd242d7c7ae3c4bed7ae98ba407596bbbc9c
davipythonweb <galaxys3tab32@gmail.com>
2025-08-18 19:45:09 -0300

    update class 17 `conjuntos`

---
+++ python-linuxTIPS/python-base/medio/class_17.py
@@ -0,0 +1,12 @@
+# AULA 17 - LInuxTipss
+# tipo de dado chamado Sets , ou CONJUNTOS.
+
+#  quantos metodos há no set: len(dir(set)) = 57 metodos
+
+
+# criando um set:
+# c1 = set( (1, 2, 3) )
+# ou 
+# c1 = set( [, 2, 3])
+
+
