64af259fd33fc0afe03bd2d4e30aa74bd8e5b29f
davipythonweb <galaxys3tab32@gmail.com>
2025-08-18 21:36:48 -0300

    update class 17 sobre set

--- python-linuxTIPS/python-base/medio/class_17.py
+++ python-linuxTIPS/python-base/medio/class_17.py
@@ -15,4 +15,18 @@
 # c2 = set("banana")
 # c2= {'a', 'b', 'n'}
 
-# FOI RETIRADO DO CONJUNTO TODOS OS REPETIDOS. 
+# no exemplo acima: FOI RETIRADO DO CONJUNTO TODOS OS REPETIDOS. 
+
+# exemplo de união de set:
+
+# CRIANDO DUAS LISTAS:
+# conjunto_a = [1, 2, 3, 4, 5]
+# conjunto_b = [4, 5, 6, 7, 8]
+
+# CONVERTENDO AS LISTAS PARA CONJUNTO E FAZENDO A UNIÂO.
+# set(conjunto_a) | set(conjunto_b)
+# {1, 2, 3, 4, 5, 6, 7, 8}
+
+# ou usando o metodo union:
+# set(conjunto_a).union(set(conjunto_b))
+# {1, 2, 3, 4, 5, 6, 7, 8}
