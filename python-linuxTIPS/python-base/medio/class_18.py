ff13e5c0ce8b094eb08afb514f56aca1855285a4
davipythonweb <galaxys3tab32@gmail.com>
2025-08-19 23:46:24 -0300

    update class 18 dict

--- python-linuxTIPS/python-base/medio/class_18.py
+++ python-linuxTIPS/python-base/medio/class_18.py
@@ -2,3 +2,9 @@
 
 # DIcionarios
 
+#  quantos metodos há no dict: len(dir(dict)) = 46 metodos
+
+# os DICIONARIOS são conjuntos de chave e valor.
+
+# exemplo:
+#  animals = {'gato': 1, 'cachorro': 2, 'passaro': 3}
