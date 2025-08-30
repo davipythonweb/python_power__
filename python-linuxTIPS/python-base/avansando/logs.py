2057abd788533976d341e5fa8f43362cc0642d34
davipythonweb <galaxys3tab32@gmail.com>
2025-08-30 15:00:38 -0300

    update class logs

--- python-linuxTIPS/python-base/avansando/logs.py
+++ python-linuxTIPS/python-base/avansando/logs.py
@@ -3,16 +3,34 @@
 
 import logging
 
-# PODE SER CUSTOMIZADO:
-    # level do erro
-    # formatar o erro (É preciso criar uma nova instancia)
-    # destino do erro
+# Handler eh a classe responsavel pelo destino
+# de onde o log sera impresso
 
-logging.debug("Mensagem pro dev.")
-logging.info("Mensagem geral para usuarios")
-logging.warning("Aviso que nao causa erro.")
-logging.error("Erro que afeta uma unica execucao,ou usuario")
-logging.critical("Erro geral , ex: banco de dados sumiu")
+# PODE SER CUSTOMIZADO:
+    # nova instancia
+log = logging.Logger("__name__", logging.DEBUG)
+    # level do log
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG) 
+    # formatar o log
+format_log = logging.Formatter(
+    '%(asctime)s %(names)s %(levelname)s l:%(lineno)d f:%(filename)s: %(message)s'
+)
+    # destino do log
+
+
+
+# logging.debug("Mensagem pro dev.")
+# logging.info("Mensagem geral para usuarios")
+# logging.warning("Aviso que nao causa erro.")
+# logging.error("Erro que afeta uma unica execucao,ou usuario")
+# logging.critical("Erro geral , ex: banco de dados sumiu")
+
+log.debug("Mensagem pro dev.")
+log.info("Mensagem geral para usuarios")
+log.warning("Aviso que nao causa erro.")
+log.error("Erro que afeta uma unica execucao,ou usuario")
+log.critical("Erro geral , ex: banco de dados sumiu")
 
 logging.critical("DEU PROBLEMA GERAL!")
 
