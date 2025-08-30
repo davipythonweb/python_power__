cb8a3625479505264337bffaf26dfaa7d287b4c6
davipythonweb <galaxys3tab32@gmail.com>
2025-08-29 23:30:11 -0300

    update class of errors

--- python-linuxTIPS/python-base/avansando/errors.py
+++ python-linuxTIPS/python-base/avansando/errors.py
@@ -2,5 +2,16 @@
 
 # a instrução abre o arquivo e o lê para
 #  imprimir a quinta linha no arquivo
+
+import sys
+
 names = open("names.txt").readlines()
-print(names[4])
+
+# Existem duas abordagens para resolver erros:
+# PRIMEIRA: LBYL => Look Before You Leap : Olhe antes de atravessar a rua, ou de pular
+
+if len(names) >= 3:
+    print(names[4])
+else:
+    print("O nome nao esta na lista!")
+    sys.exit(1)
