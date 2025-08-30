24d3d573cb90bb5ca1e166e1d425f67a070a5d7a
davipythonweb <galaxys3tab32@gmail.com>
2025-08-30 02:18:00 -0300

    update class erors

--- python-linuxTIPS/python-base/avansando/errors.py
+++ python-linuxTIPS/python-base/avansando/errors.py
@@ -2,13 +2,20 @@
 
 # a instrução abre o arquivo e o lê para
 #  imprimir a quinta linha no arquivo
-
+import os
 import sys
 
-names = open("names.txt").readlines()
-
-# Existem duas abordagens para resolver erros:
 # PRIMEIRA: LBYL => Look Before You Leap : Olhe antes de atravessar a rua, ou de pular
+
+# LBYL => Look Before You Leap
+if os.path.exists("names.txt"):
+    names = open("names.txt").readlines()
+else:
+    print("[ERROR] Missing name in the list!")
+    sys.exit(1)
+
+
+# LBYL => Look Before You Leap
 
 if len(names) >= 3:
     print(names[2])
