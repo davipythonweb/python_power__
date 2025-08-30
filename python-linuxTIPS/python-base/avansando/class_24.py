343342e867a9af4052d4bd62d769ec89a1575942
davipythonweb <galaxys3tab32@gmail.com>
2025-08-30 03:14:40 -0300

    update class errors

--- python-linuxTIPS/python-base/avansando/class_24.py
+++ python-linuxTIPS/python-base/avansando/class_24.py
@@ -3,3 +3,15 @@
 
 # tratamentos de erros com Excessões
 
+
+# Existem duas abordagens para resolver erros:
+# PRIMEIRA: LBYL => Look Before You Leap : Olhe antes de atravessar a rua, ou de pular
+# OBS: Estas OPERAÇOES de IO(input/output)
+# podem demorar. Exemplo, usar(if,else) para verificar errors.
+# ESTA OPÇAO causa o (Race Condition) , corrida de leitura entre arquivos, por exemplo doi programas que acessao o mesmo arquivo.
+
+
+# ===>ESTA ABORDAGEM para errors EH A MELHOR!<=====
+# SEGUNDA: EAFP => Easy to Ask Forgiveness than permission : É mais fácil pedir perdão do que permissão.
+
+# usa-se o try , except
--- python-linuxTIPS/python-base/avansando/errors.py
+++ python-linuxTIPS/python-base/avansando/errors.py
@@ -8,20 +8,49 @@ import sys
 
 # Existem duas abordagens para resolver erros:
 # PRIMEIRA: LBYL => Look Before You Leap : Olhe antes de atravessar a rua, ou de pular
+# OBS: Estas OPERAÇOES de IO(input/output)
+# podem demorar. Exemplo, usar(if,else) para verificar errors.
+# ESTA OPÇAO causa o (Race Condition) , corrida de leitura entre arquivos, por exemplo doi programas que acessao o mesmo arquivo.
+
 
 # LBYL => Look Before You Leap
 # VERIFICANDO SE EXISTE O ARQUIVO.
+"""
 if os.path.exists("names.txt"):
+    input("...") # Race Condition
     names = open("names.txt").readlines()
 else:
     print("[ERROR 1] File names.txt not found!")
     sys.exit(1)
+"""
 
-    
 # LBYL => Look Before You Leap
 # VERIFICANDO O NUMERO DE LINHAS
+"""
 if len(names) >= 3:
     print(names[2])
 else:
+    print("[ERROR 2] Missing name in the list!")
+    sys.exit(1)
+
+"""
+
+# ===>ESTA ABORDAGEM para errors EH A MELHOR!<=====
+# SEGUNDA: EAFP => Easy to Ask Forgiveness than permission : É mais fácil pedir perdão do que permissão. usa-se o try, except.
+
+
+# EAFP => Easy to Ask Forgiveness than permission
+# VERIFICANDO SE EXISTE O ARQUIVO.
+try:
+    names = open("names.txt").readlines()
+except:
+    print("[ERROR 1] File names.txt not found!")
+    sys.exit(1)
+
+# EAFP => Easy to Ask Forgiveness than permission
+# VERIFICANDO O NUMERO DE LINHAS
+try:
+    print(names[2])
+except:
     print("[ERROR 2] Missing name in the list!")
     sys.exit(1)
