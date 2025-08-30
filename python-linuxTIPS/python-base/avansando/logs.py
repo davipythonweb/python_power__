e7f3ecd250bd87c7d96489aa3bb64bcf7b4442b4
davipythonweb <galaxys3tab32@gmail.com>
2025-08-30 14:38:18 -0300

    update class logs

--- python-linuxTIPS/python-base/avansando/logs.py
+++ python-linuxTIPS/python-base/avansando/logs.py
@@ -3,6 +3,17 @@
 
 import logging
 
+# PODE SER CUSTOMIZADO:
+    # level do erro
+    # formatar o erro (É preciso criar uma nova instancia)
+    # destino do erro
+
+logging.debug("Mensagem pro dev.")
+logging.info("Mensagem geral para usuarios")
+logging.warning("Aviso que nao causa erro.")
+logging.error("Erro que afeta uma unica execucao,ou usuario")
+logging.critical("Erro geral , ex: banco de dados sumiu")
+
 logging.critical("DEU PROBLEMA GERAL!")
 
 """
