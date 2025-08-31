fe6920aaff7565d73947e9f6295be51ade716955
davipythonweb <galaxys3tab32@gmail.com>
2025-08-30 22:49:15 -0300

    update class 26

--- python-linuxTIPS/python-base/avansando/algoritmos/class_26.py
+++ python-linuxTIPS/python-base/avansando/algoritmos/class_26.py
@@ -1,3 +1,22 @@
 # aula 26 - linux-Tips
 
-# algoritmos
+# rabbit hole
+
+# algoritmos:
+# SEQUENCIA DE INSTRUÇOES LOGICASQUE VISÂO OBTER
+# A SOLUÇÂO DE UM PROBLEMA.
+
+# PROBLEMA: Ir a Padaria e comprar Pão:
+# PREMISSA:Padaria da esquina abre fds:até 12h, semana 
+# até 19h, feriado(exceto natal) não abre.
+
+# 1-A padaria está aberta:
+    # 1- Se é feriado E NÂO é natal: não
+    # 2- Senão, Se é sabado OU domingo E antes do  meio dia: sim
+    # 3- Senão, Se é dia de semana E antes das 19h: sim
+
+# 2-
+
+# 4-
+
+# 3-
