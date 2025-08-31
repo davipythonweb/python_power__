e775aeabf94f69756e697503caa74195aca1392c
davipythonweb <galaxys3tab32@gmail.com>
2025-08-31 00:07:12 -0300

    update class 26

--- python-linuxTIPS/python-base/avansando/algoritmos/class_26.py
+++ python-linuxTIPS/python-base/avansando/algoritmos/class_26.py
@@ -14,9 +14,17 @@
     # 1- Se é feriado E NÂO é natal: não
     # 2- Senão, Se é sabado OU domingo E antes do  meio dia: sim
     # 3- Senão, Se é dia de semana E antes das 19h: sim
+    # 4- Senão: não
 
-# 2-
+# 2- Se padaria esta aberta E:
+    # 1- se esta chovendo: pegar guarda chuva
+    # 2- Se esta chovendo E calor: pegar guarda chuva e agua
+    # 3-  Se esta chovendo E frio OU nevando:pegar guardaa chuva blusa e botas
 
-# 4-
+# 4- Ir até a padaria:
+    # 1- Se tem pao integral E baguete: pedir 6 de cada
+    # 2- Senão, Se tem apenas pao integral OU baguete:pedir 12
+    # 3- Senão: pedir 6 de qualquer um
 
-# 3-
+# 3-Senão:
+    # 1- Ficar em casa E comer Bolachas
