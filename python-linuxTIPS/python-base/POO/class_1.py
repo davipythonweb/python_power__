7e3c28cbdff3a74ad688fa3493681b2eeeb56146
davipythonweb <galaxys3tab32@gmail.com>
2025-11-26 00:40:45 -0300

    testando imperativa com funcional

--- python-linuxTIPS/python-base/POO/class_1.py
+++ python-linuxTIPS/python-base/POO/class_1.py
@@ -1,12 +1,9 @@
 # POO - PROGRAMAÇAO ORIENTADA A OBJETOS com PYTHON
 
-# PARADIGMA IMPERATIVO ou PROCIDURAL
-
-
+# PARADIGMA[forma como a gente se expressa e declara as coisas no nosso programa] IMPERATIVO ou PROCIDURAL
 # exemplo para iniciar a aula de POO
 
 # exemplo de programaçao com paradigma imperativo
-
 people = [
     {
         "name":"Eliote Alderson",
@@ -20,12 +17,33 @@ people = [
     }
 ]
 
+# def add_points(person, value):
+#     if person["role"] == "Manager":
+#         value *=  2
+#     person["balance"] += value
+
+# for person in people:
+#     add_points(person, 100)
+    
+
+# print(people)
+
+
+# misturando paradigma imperativo com funcional
+# a programaçao funcional, são puras= no side effects
 def add_points(person, value):
-    if person["role"] == "Manager":
+    data = person.copy() # aqui seria o (no side effects, porque uma copia do objeto['person'] sera mudado), a "data".
+    if data["role"] == "Manager":
         value *=  2
-    person["balance"] += value
+    data["balance"] += value
+    return data
+  
+    
+result = map(lambda person: add_points(person, 100), people)
+# funcional
+# declarativa
+# lazy valuation = ou seja,  sao avaliadas somente quanto eh consumido o objeto.
 
-for person in people:
-    add_points(person, 100)
-
+# consindo o map , passando para um iterator(objeto capaz de consumir um iterador) , no caso o list.
+print(list(result))
 print(people)
