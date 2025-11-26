73e8aa990bdc9d46d57df319e37889bdfa121de1
davipythonweb <galaxys3tab32@gmail.com>
2025-11-25 23:44:04 -0300

    create class_1 of poo

---
+++ python-linuxTIPS/python-base/POO/class_1.py
@@ -0,0 +1,22 @@
+# POO - PROGRAMAÇAO ORIENTADA A OBJETOS com PYTHON
+
+# PARADIGMA IMPERATIVO ou PROCIDURAL
+
+
+# exemplo para iniciar a aula de POO
+
+# exemplo de forma imperativa
+
+people = [
+    {
+        "name":"Eliote Alderson",
+        "balance":500,
+        "role":"engenier"
+    },
+    {
+        "name":"Roger",
+        "balance":450,
+        "role":"manager"
+    }
+]
+
