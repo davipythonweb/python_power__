0fabfc856256ab7344e1c78586c3e45896e7a048
davipythonweb <galaxys3tab32@gmail.com>
2025-11-28 00:14:50 -0300

    update class3

--- python-linuxTIPS/python-base/POO/class_3.py
+++ python-linuxTIPS/python-base/POO/class_3.py
@@ -1,23 +1,35 @@
 class Person: # Nas classes usa-se: pascalCase , UpperCamelCase
     """ Representa uma pessoa"""
+    # aqui os atributos de classe imutaveis
     company_name = "Dunder Mifflin" # variaveis usa-se: snake_Case
     work_address = "Rua Stanton. Pensilvania"
-    balance = 0
+    balance = 0 # atributo de classe imutavel
+
+    # Python Data Model - Metodo Dunder ou Metodos Mágicos
+    # metodo __init__() eh o inicializador da classe
+    # aqui os atributos de classe mutaveis
+    def __init__(person, name):
+        person.name = name
+
+    # nunca defina um valor mutavel como atributo de classe!
+    prefered_colors = []  # atributo de classe mutavel
 
     # Nas funçoes dentro das classes[metodos] usa-se: snake_Case
+    # Injeçao de dependencia -1 arg metodo = a propria instancia
     def add_points(person, value): # funçao dentro de uma classe eh um: Método.
         if person.role == "Manager":
             value *= 2
         person.balance += value
 
-jim = Person()
-jim.name = "Jim Halpert"
+jim = Person(name="Jim Halpert")
 jim.role = "Salesman"
 jim.add_points(100)
-print(jim.name, jim.balance)
+jim.prefered_colors.append("Blue")
+jim.work_address = "Home"
+print(jim.name, jim.balance, jim.prefered_colors, jim.work_address)
 
-pam = Person()
-pam.name = "Pam Besly"
+pam = Person(name="Pam Besly")
 pam.role = "Receptionist"
 pam.add_points(100)
-print(pam.name, pam.balance)
+pam.prefered_colors.append("Purple")
+print(pam.name, pam.balance, pam.prefered_colors, pam.work_address)
---
+++ python-linuxTIPS/python-base/POO/readme.md
@@ -0,0 +1,3 @@
+<!-- rodando o arquivo apos salvar com a biblioteca watchdogs -->
+
+- pip install watchdogs
