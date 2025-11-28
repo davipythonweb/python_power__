3ff30ce604b5120c84c5ab1d79c65c7e1e9888f2
davipythonweb <galaxys3tab32@gmail.com>
2025-11-27 22:10:19 -0300

    create class3

---
+++ python-linuxTIPS/python-base/POO/class_3.py
@@ -0,0 +1,22 @@
+class Person: # Nas classes usa-se: pascalCase , UpperCamelCase
+    """ Representa uma pessoa"""
+    company_name = "Dunder Mifflin" # variaveis usa-se: snake_Case
+    work_address = "Rua Stanton. Pensilvania"
+    balance = 0
+
+    # Nas funçoes dentro das classes[metodos] usa-se: snake_Case
+    def add_points(person, value): # funçao dentro de uma classe eh um: Método.
+        if person.role == "Manager":
+            value *= 2
+        person.balance += value
+
+jim = Person()
+jim.name = "Jim Halpert"
+jim.role = "Salesman"
+jim.add_points(100)
+
+pam = Person()
+pam.name = "Pam Besly"
+pam.role = "Receptionist"
+pam.add_points(100)
+print(pam.name, pam.balance)
