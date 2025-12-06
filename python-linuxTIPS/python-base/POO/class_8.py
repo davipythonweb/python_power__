32f8e72e93ca3e243ac7bcd94afada60be3c130b
davipythonweb <galaxys3tab32@gmail.com>
2025-12-06 01:47:27 -0300

    update class_8

--- python-linuxTIPS/python-base/POO/class_8.py
+++ python-linuxTIPS/python-base/POO/class_8.py
@@ -1,4 +1,4 @@
-# exemplo encapsulamento2
+# exemplo2 encapsulamento
 
 class Conta:
     _tipo_de_conta = 'Corrente'  # Atributo (protegido)
@@ -6,13 +6,26 @@ class Conta:
     def __init__(self, cliente):
         self.cliente = cliente
         self._saldo = 0  # Atributo (protegido)
+    
+    # encapsulando o atributo _saldo dentro dos métodos da classe
+    def depositar(self, valor):
+        self._saldo += valor
 
-    def conssultar(self):
+    def sacar(self, valor):
+        if valor > self._saldo:
+            print('Saldo insuficiente')
+        else:
+            self._saldo -= valor
+
+    def consultar(self):
         if self._saldo <= 0:
             print('Saldo insuficiente')
         return self._saldo
     
 
 conta = Conta(cliente='Eliote')
+conta.depositar(500)
+print(conta.consultar())
 
-print(conta.conssultar())
+conta.sacar(100)
+print(conta.consultar())
