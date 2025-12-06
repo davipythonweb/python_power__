847174b9915e4b45129a80920f7327f5b0e8bf99
davipythonweb <galaxys3tab32@gmail.com>
2025-12-06 00:20:52 -0300

    create class_7

---
+++ python-linuxTIPS/python-base/POO/class_7.py
@@ -0,0 +1,13 @@
+# POO
+
+"""Abstraçao: Capacidade de abstrair implementaçoes."""
+
+""" Herança:Capacidade de herdar de outras classes"""
+
+""" Polimorfismo: Capacidade de uma implementação
+    se comportar de maneira similar independente
+    da dorma do objeto"""
+
+""" Encapsulamento: Capacidade de um objeto esconder a
+    sua implementação interna e expor , apenas 
+    o que for conveniente."""
