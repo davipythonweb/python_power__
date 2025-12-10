8580710f0161a3eaac8e560c24b586a695c99ed8
davipythonweb <galaxys3tab32@gmail.com>
2025-12-10 03:12:37 -0300

    create class of protocols and Data Model

---
+++ python-linuxTIPS/python-base/POO/protocolos & Data Model_10.py
@@ -0,0 +1,12 @@
+# POO
+
+# Protocolos / Data Model
+# protocolos são um conjunto de regras e convenções que definem como os objetos interagem entre si em Python.
+# Eles são implementados através de métodos especiais (métodos mágicos) que começam e terminam com dois underlines (__).
+# Esses métodos permitem que os objetos se comportem de maneira específica em determinadas situações, como operações
+# aritméticas, comparações, iteração, entre outras.
+# Exemplos comuns de métodos especiais incluem:
+# __init__: Inicializa um objeto.
+# __str__: Define a representação em string de um objeto.
+# __add__: Define o comportamento do operador de adição (+) para objetos.
+# __len__: Retorna o comprimento de um objeto.
