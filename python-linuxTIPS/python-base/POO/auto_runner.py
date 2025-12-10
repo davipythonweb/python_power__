31cd2617658aabc783bf7a3da4ae9403d620f8de
davipythonweb <galaxys3tab32@gmail.com>
2025-12-10 03:48:16 -0300

    update aula protocolos

--- python-linuxTIPS/python-base/POO/auto_runner.py
+++ python-linuxTIPS/python-base/POO/auto_runner.py
@@ -4,7 +4,7 @@ from watchdog.events import FileSystemEventHandler # type: ignore
 import time
 import os
 
-FILE_TO_WATCH = "encapsulamento3_9.py"
+FILE_TO_WATCH = "protocolos & Data Model_10.py"
 
 class Handler(FileSystemEventHandler):
     def on_modified(self, event):
--- python-linuxTIPS/python-base/POO/protocolos & Data Model_10.py
+++ python-linuxTIPS/python-base/POO/protocolos & Data Model_10.py
@@ -1,12 +1,49 @@
 # POO
 
 # Protocolos / Data Model
+
+
 # protocolos são um conjunto de regras e convenções que definem como os objetos interagem entre si em Python.
+
 # Eles são implementados através de métodos especiais (métodos mágicos) que começam e terminam com dois underlines (__).
+
 # Esses métodos permitem que os objetos se comportem de maneira específica em determinadas situações, como operações
 # aritméticas, comparações, iteração, entre outras.
+
+# Ao implementar esses métodos em suas classes, você pode definir como os objetos dessas classes devem se comportar
+# em diferentes contextos, tornando-os mais integrados e compatíveis com o restante do ecossistema Python.
+
 # Exemplos comuns de métodos especiais incluem:
 # __init__: Inicializa um objeto.
 # __str__: Define a representação em string de um objeto.
 # __add__: Define o comportamento do operador de adição (+) para objetos.
 # __len__: Retorna o comprimento de um objeto.
+# __repr__: Fornece uma representação oficial de um objeto.
+
+# Um dos protocolos mais faceis de entender -> Printable
+
+class Cor: # Base Class
+    #  icon de quadrado branco
+    icon = "⬜​​"
+    def __str__(self):
+        return self.icon
+
+class Amarelo(Cor):
+    # icon com quadrado amarelo
+    icon = "🟨​"
+
+class Azul(Cor):
+    # icon com quadrado de cor azul
+    icon = "🟦​"
+
+#  emotion de quadrado amarelo
+#  emotion = "\U0001F7E1"
+
+class Vermelho(Cor):
+    # icon com quadrado vermelho
+    icon = "🟥​"
+
+print("Cores Primarias")
+print(Amarelo())
+print(Azul())
+print(Vermelho())
