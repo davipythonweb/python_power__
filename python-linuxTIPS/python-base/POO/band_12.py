cca8880d4cad6ff9470a43648a2cc0aa0c64d34a
davipythonweb <galaxys3tab32@gmail.com>
2025-12-22 23:59:39 -0300

    add colors attribute to Guitar and Flute instances

--- python-linuxTIPS/python-base/POO/band_12.py
+++ python-linuxTIPS/python-base/POO/band_12.py
@@ -1,10 +1,9 @@
 from instruments_12 import Guitar, Flute, InstrumentKind
 
-gianini = Guitar(name="Gianini", kind=InstrumentKind.string)
+gianini = Guitar(name="Gianini", kind=InstrumentKind.string, colors=["green"])
 print(gianini.play())  # Output: Trom Trom
-print(gianini.colors)  # Output: []
+print(gianini.colors)  # Output: ['green']
 
-yamaha = Flute(name="Yamaha")
+yamaha = Flute(name="Yamaha", kind=InstrumentKind.wind, colors=["silver", "gold"])
 print(yamaha.play())   # Output: Tooooot
-print(yamaha.colors)   # Output: []
-
+print(yamaha.colors)   # Output: ['silver', 'gold']
