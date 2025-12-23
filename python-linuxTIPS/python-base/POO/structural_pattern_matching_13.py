3adb1a351ad98fab5fad9c4530b83ba93a381915
davipythonweb <galaxys3tab32@gmail.com>
2025-12-23 18:35:49 -0300

    create class 13

---
+++ python-linuxTIPS/python-base/POO/structural_pattern_matching_13.py
@@ -0,0 +1,4 @@
+# Pattern Match Estrutural
+# Novidade do Python 3.10
+# https://docs.python.org/3/tutorial/controlflow.html#match-statements
+# https://docs.python.org/3/reference/compound_stmts.html#the-match-statement
