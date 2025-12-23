fc06b1123e5050de6438b23c24d237fd632e7d19
davipythonweb <galaxys3tab32@gmail.com>
2025-12-23 18:47:24 -0300

    create tela com class Turtle

--- python-linuxTIPS/python-base/POO/structural_pattern_matching_13.py
+++ python-linuxTIPS/python-base/POO/structural_pattern_matching_13.py
@@ -2,3 +2,20 @@
 # Novidade do Python 3.10
 # https://docs.python.org/3/tutorial/controlflow.html#match-statements
 # https://docs.python.org/3/reference/compound_stmts.html#the-match-statement
+
+from turtle import Turtle
+
+turtle = Turtle()
+turtle.shape("turtle")
+turtle.speed(3)
+turtle.color("blue", "yellow")
+
+while True:
+
+# icone de tartaruga no input
+    command = input("🐢>").strip()
+    if command == "exit":
+        break
+    if command == "draw":
+        turtle.pendown()
+        turtle.forward(20)
