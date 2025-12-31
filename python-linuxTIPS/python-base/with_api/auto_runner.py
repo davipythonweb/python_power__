00d3b3559d7e4ab1fa93a70221192d5b1c691064
davipythonweb <galaxys3tab32@gmail.com>
2025-12-31 20:38:23 -0300

    create class with api

---
+++ python-linuxTIPS/python-base/with_api/auto_runner.py
@@ -0,0 +1,31 @@
+import subprocess
+from watchdog.observers import Observer # type: ignore
+from watchdog.events import FileSystemEventHandler # type: ignore
+import time
+import os
+
+FILE_TO_WATCH = "teste.py"
+
+class Handler(FileSystemEventHandler):
+    def on_modified(self, event):
+        if event.src_path.endswith(FILE_TO_WATCH):
+            print("\n🔄 Arquivo salvo! Executando...\n")
+            subprocess.run(["python", FILE_TO_WATCH])
+
+event_handler = Handler()
+observer = Observer()
+observer.schedule(event_handler, ".", recursive=False)
+observer.start()
+
+print("👀 Monitorando alterações no arquivo...")
+
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    observer.stop()
+
+observer.join()
+
+
+# codigo para observar um arquivo e fazer as mudanças no documento e ja rodar o script python, apos ctrl + s
